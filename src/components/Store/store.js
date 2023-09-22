0fb7b860f21cf6554b333aa661912ff85ceb48da
AbdullahJaveed <m.abdullahhjaveed@gmail.com>
2023-09-22 23:50:01 +0500

    Feat : Intregrate Redux persist in store & app.js

--- src/components/Store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import pricingReducer, { updatePricingCart } from "./pricingSlice";
-
-export const store = configureStore({
-  reducer: {
-    pricing: pricingReducer,
-  },
-});
